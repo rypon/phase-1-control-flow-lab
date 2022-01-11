036ec82a3e9202953b198ddf0b2aa8718d3daacb
Ryan O'Neill <ryanponeill00@gmail.com>
2022-01-10 20:06:34 -0500

    commit tree 4662386885f3ed9b42107f657dc0b132c45a96fd

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someAmount) {
   // Write your code here!
+
+  if (someAmount <= 400) {
+    return 'This one is on me!'
+  } else if (someAmount > 2000 && someAmount < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (someAmount > 2500) {
+    return 'No can do.'
+  }
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(someCity) {
   // Write your code here!
+  return someCity === 'NYC' ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(someTip) {
   // Write your code here!
+  switch (someTip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.'
+  }
 }
